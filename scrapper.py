a83334e988bab832cc45154339a11d2c8b3b35e8
Alex <mwppl123@gmail.com>
2022-12-03 16:39:11 +0100

    Working all pages

--- scrapper.py
+++ scrapper.py
@@ -10,23 +10,24 @@ def find_last_page():
     soup = BeautifulSoup(request.content , 'html5lib')
     ul = soup.find('ul', {'class': 's_paging__item pagination mb-2 mb-sm-3'})
     li = ul.find_all('li', {'class' : 'pagination__element --item'})
-    last_page = li[-1].text
+    last_page = int(li[-1].text)
     return last_page
 
 def main(last_page):
     counter = 1
-    URL = "https://www.ezebra.pl/pl/promotions/promocja.html?&filter_traits%5B25445%5D=25451%2C25464%2C25461%2C25450%2C25455&filter_price=0-30"
-    request = requests.get(URL)
-    soup = BeautifulSoup(request.content , 'html5lib')
-    div = soup.find_all("div", {'class': 'product col-6 col-sm-4 pt-3 pb-md-3'})
-    for element in div:
-        a = element.find('a', {'class':'product__icon d-flex justify-content-center align-items-center'})
-        href = a['href']
-        sale = element.find("div", {'class': 'product__yousavepercent'}).text.strip()
-        first_price = element.find('del', {"class": "price --max"}).text
-        sale_price = element.find('strong', {'class' : 'price --max-exists'}).text
-        print(f"{counter}. {first_price} {sale} = {sale_price}")
-        counter += 1
+    for page in range(0,last_page):
+        URL = f"https://www.ezebra.pl/pl/promotions/promocja.html?&filter_traits%5B25445%5D=25451%2C25464%2C25461%2C25450%2C25455&filter_price=0-30&counter={page}"
+        request = requests.get(URL)
+        soup = BeautifulSoup(request.content , 'html5lib')
+        div = soup.find_all("div", {'class': 'product col-6 col-sm-4 pt-3 pb-md-3'})
+        for element in div:
+            a = element.find('a', {'class':'product__icon d-flex justify-content-center align-items-center'})
+            href = a['href']
+            sale = element.find("div", {'class': 'product__yousavepercent'}).text.strip()
+            first_price = element.find('del', {"class": "price --max"}).text
+            sale_price = element.find('strong', {'class' : 'price --max-exists'}).text
+            print(f"{counter}. {first_price} {sale} = {sale_price}")
+            counter += 1
 
 last_page = find_last_page()
 main(last_page)
