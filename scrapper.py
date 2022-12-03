f0411e88487d3c10f36b1c8f1f8d8b0ee91e39cf
Alex <mwppl123@gmail.com>
2022-12-03 16:28:42 +0100

    Second take, this time ezebra, scrapping from single page

--- scrapper.py
+++ scrapper.py
@@ -1,70 +1,26 @@
 from bs4 import BeautifulSoup
 import sqlite3 as sql
 import requests
-#Rossman categories 
-face = "8686"
-makeup = "8528"
-hair = "8655"
-body = "8625"
-perfumes = "8512"
+#https://www.ezebra.pl/pl/promotions/promocja.html?&filter_traits%5B25445%5D=25451%2C25464%2C25461%2C25450%2C25455&filter_price=0-30
 
-def find_last_page(category):
-    URL = f"https://www.rossmann.pl/promocje?CategoryId={category}&Page=1&PageSize=96"
-    request = requests.get(URL)
-    soup = BeautifulSoup(request.content, 'html5lib')
-    a = soup.find('a', {'class':'pages__last'})
-    last_page = int(a.text)
+
+def find_last_page():
+    last_page = 0
     return last_page
 
-def main(category, last_page):
-    counter = 0
-    for page in range(last_page):
-        URL = f"https://www.rossmann.pl/promocje?CategoryId={category}&Page={page}&PageSize=96"
-        request = requests.get(URL)
-        soup = BeautifulSoup(request.content, 'html5lib')
-        a = soup.find_all('a', {'class': "tile-product__name"})
-        for element in a:
-            link = "https://rossmann.pl" + element['href']
-            print(f"{counter}. {link}")
-            counter += 1
-            single_sale(link)
-
-def single_sale(URL):
+def main():
+    counter = 1
+    URL = "https://www.ezebra.pl/pl/promotions/promocja.html?&filter_traits%5B25445%5D=25451%2C25464%2C25461%2C25450%2C25455&filter_price=0-30"
     request = requests.get(URL)
-    soup = BeautifulSoup(request.content, "html5lib")
-    span = soup.find('small', {"class" : "h2 d-block text-primary"})
-    #strong = small.find_all ('strong')
-    print (span)
+    soup = BeautifulSoup(request.content , 'html5lib')
+    div = soup.find_all("div", {'class': 'product col-6 col-sm-4 pt-3 pb-md-3'})
+    for element in div:
+        a = element.find('a', {'class':'product__icon d-flex justify-content-center align-items-center'})
+        href = a['href']
+        sale = element.find("div", {'class': 'product__yousavepercent'}).text.strip()
+        first_price = element.find('del', {"class": "price --max"}).text
+        sale_price = element.find('strong', {'class' : 'price --max-exists'}).text
+        print(f"{counter}. {first_price} {sale} = {sale_price}")
+        counter += 1
 
-
-def menu():
-    print("""
-    PLEASE CHOOSE YOUR CATEGORY
-    
-    1. FACE
-    2. MAKEUP
-    3. HAIR
-    4. BODY
-    5. PERFUMES
-    
-    TYPE IN CORESPONDING NUMBER""")
-    choice = input(">> ")
-    if choice == "1":
-        last_page = find_last_page(face)
-        main(face, last_page)
-    elif choice == "2":
-        last_page = find_last_page(makeup)
-        main(makeup, last_page)
-    elif choice == "3":
-        last_page = find_last_page(hair)
-        main(hair, last_page)
-    elif choice == "4":
-        last_page = find_last_page(body)
-        main(body, last_page)
-    elif choice == "5":
-        last_page = find_last_page(perfumes)
-        main(perfumes, last_page)
-    else:
-        print("Thats an error")
-
-menu()
+main()
