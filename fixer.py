062b0298d3caa75fb2eff91004359405b78bc38f
Alex <mwppl123@gmail.com>
2022-12-24 00:41:50 +0100

    Fixing DB

--- fixer.py
+++ fixer.py
@@ -5,7 +5,11 @@ import sqlite3 as sql
 conn = sql.connect("shopsales/sales.db")
 cur = conn.cursor()
 
-query = cur.execute("SELECT product_id, link from sales WHERE product_id NOT IN (SELECT product_id FROM stock)")
+query = cur.execute("SELECT product_id from sales WHERE product_id NOT IN (SELECT product_id FROM stock)")
 
 for element in query:
-    print(element[1])
+    print(f"{element}...", end=" ")
+    cur.execute('DELETE FROM sales WHERE product_id = ?', element)
+    print("Done")
+conn.commit()
+    
